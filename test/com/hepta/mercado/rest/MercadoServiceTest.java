074025d5e04922db07d0c06e0e0588939203c1a6
Márcio Alexandre <Freire Sindeaux>
2019-12-15 07:14:06 -0300

    feat: Criação de novos Testes

--- test/com/hepta/mercado/rest/MercadoServiceTest.java
+++ test/com/hepta/mercado/rest/MercadoServiceTest.java
@@ -1,32 +1,30 @@
 package com.hepta.mercado.rest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import javax.persistence.EntityManager;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Temporal;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
 import com.hepta.mercado.builder.FabricanteBuilder;
 import com.hepta.mercado.builder.ProdutoBuilder;
 import com.hepta.mercado.entity.Produto;
 import org.glassfish.jersey.client.ClientConfig;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MercadoServiceTest {
 
 	private static WebTarget service;
 	private static final String URL_LOCAL = "http://localhost:8080/mercado/rs/produtos";
-	
+
+	private static Produto produto;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		ClientConfig config = new ClientConfig();
@@ -34,15 +32,9 @@ class MercadoServiceTest {
 		service = client.target( UriBuilder.fromUri(URL_LOCAL).build() );
 	}
 
-	@Test
-	void testListaTodosProdutos() {
-		Response response = service.request().get();
-		assertEquals(response.getStatusInfo().getStatusCode() , Response.Status.OK.getStatusCode());
-	}
-
-	@Test
-	public void testCadastrarNovoProduto(){
-		Produto produto = ProdutoBuilder.umProduto()
+	@BeforeEach
+	void setUpProduto(){
+		produto = ProdutoBuilder.umProduto()
 				.comVolume(Double.valueOf(2))
 				.comUnidade("Unidades")
 				.comNome("Camisa")
@@ -54,8 +46,25 @@ class MercadoServiceTest {
 								.agora()
 				)
 				.agora();
+	}
+
+	@Test
+	void testListaTodosProdutos() {
+		Response response = service.request().get();
+		assertEquals(response.getStatusInfo().getStatusCode() , Response.Status.OK.getStatusCode());
+	}
+
+	@Test
+	void testCadastrarNovoProduto(){
 		Response response = service.request().post(Entity.entity(produto, MediaType.APPLICATION_JSON_TYPE));
 		assertEquals(response.getStatusInfo().getStatusCode() ,Response.Status.OK.getStatusCode());
+	}
+
+	@Test
+	void naoDeveCadastrarNovoProdutoSemFabricante(){
+		produto.setFabricante(null);
+		Response response = service.request().post(Entity.entity(produto, MediaType.APPLICATION_JSON_TYPE));
+		assertEquals(response.getStatusInfo().getStatusCode(), Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
 	}
 
 }
