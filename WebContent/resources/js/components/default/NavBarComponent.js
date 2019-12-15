e74cba94da2d2bdb226b2f7099fa393bab20b95f
Márcio Alexandre <Freire Sindeaux>
2019-12-15 18:14:10 -0300

    fix: Retirada de tag não usada

--- WebContent/resources/js/components/default/NavBarComponent.js
+++ WebContent/resources/js/components/default/NavBarComponent.js
@@ -3,9 +3,6 @@ Vue.component('navigation',{
     template:`
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.html" style="font-size: 20px;">Mercadinho</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
-		    <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="collapse navbar-collapse" id="textoNavbar">
             <ul class="navbar-nav mr-auto" v-if="first">
                 <li class="nav-item active">
